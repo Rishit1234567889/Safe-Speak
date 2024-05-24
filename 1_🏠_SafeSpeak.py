e7c83ceec99ee9784d4804e64f31e3a88c7ea0ee
manya0210 <bishtmanya14@gmail.com>
2024-05-25 01:25:25 +0530

    Updated About Us Page

--- 1_🏠_SafeSpeak.py
+++ 1_🏠_SafeSpeak.py
@@ -15,6 +15,7 @@ footer{
 
 showWarningOnDirectExecution = False
 image = Image.open('icons/logo.png')
+main_image = Image.open('icons/img1.png')
 
 
 st.set_page_config(page_title = "SafeSpeak", page_icon = image)
@@ -30,4 +31,25 @@ st.sidebar.markdown("---")
 
 st.sidebar.markdown("<br> <br> <br> <br> <br> <br> <h1 style='text-align: center; font-size: 18px; color: #0080FF;'> © 2024 | SafeSpeak </h1>", unsafe_allow_html=True)
 
-st.image(image , use_column_width=True)
+# st.image(image , use_column_width=True)
+
+col1, col2 = st.columns([4, 4])
+
+with col1:
+    st.markdown("""
+        <h1 style="font-size: 60px; color: #0080FF; margin-top: 55px; margin-left: -150px;">SafeSpeak</h1>
+        <h3 style="font-size: 30px; margin-bottom: 20px; margin-left: -150px;">Empowering Safe Online Communication</h3>
+    """, unsafe_allow_html=True)
+
+with col2:
+    st.image(main_image, use_column_width=True, caption=None)
+
+# Additional CSS for further customization
+st.markdown("""
+    <style>
+    .css-1aumxhk { /* Class selector for the main content area */
+        padding-top: 0; /* Remove the padding at the top */
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
--- pages/5_📍_About us.py
+++ pages/5_📍_About us.py
@@ -28,7 +28,7 @@ st.sidebar.image(image , use_column_width=True, output_format='auto')
 st.sidebar.markdown("---")
 
 
-st.sidebar.markdown("<br> <br> <br> <br> <br> <br> <br> <h1 style='text-align: center; font-size: 18px; color: #0080FF;'>© 2023 | Ioannis Bakomichalis</h1>", unsafe_allow_html=True)
+st.sidebar.markdown("<br> <br> <br> <br> <br> <br> <br> <h1 style='text-align: center; font-size: 18px; color: #0080FF;'>© 2024 | SafeSpeak </h1>", unsafe_allow_html=True)
 
 
 
@@ -37,16 +37,29 @@ st.title("About us📍")
 st.markdown("---")
 st.markdown("<br>", unsafe_allow_html=True)
 
-st.markdown("The project **_CBDA_** developed by Postgraduate student :blue[**_Ioannis Bakomichalis_**] for his MSc Dissertation :blue[**_Cyberbullying Detection through NLP & Machine Learning_**].")
-st.markdown(" <br> ", unsafe_allow_html=True)
-st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("For any question about the project, please contact <a href=mailto:ioannis.bakomichalis@ssl-unipi.gr>Ioannis Bakomichalis</a> .", unsafe_allow_html=True)
-st.markdown("<br> <br> <br>", unsafe_allow_html=True)
-col1, col2 = st.columns([2,4])
-with col1:
-    image = Image.open('icons/unipiLogo.png')
-    st.image(image, use_column_width=False, output_format='auto')
-with col2:
-    st.markdown("<br> ", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center; font-size: 15px; color: #002d51;'>University of Piraeus, Department of Digital Systems</h1>", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center; font-size: 15px; color: #002d51;'>M.Sc. Digital Systems Security</h1>", unsafe_allow_html=True)
+st.markdown("""
+<div style="font-size: 20px;">
+    SafeSpeak's mission is to empower safe online communication by detecting and mitigating cyberbullying. We are committed to creating a safer internet where everyone can communicate without fear of harassment.
+    <br><br>
+    Developed by a dedicated team of professionals, SafeSpeak leverages advanced machine learning and natural language processing (NLP) techniques to identify cyberbullying in real-time.
+    <br><br>
+    We are always looking for feedback and ways to improve. Reach out to us at <a href="mailto:info@safespeak.com">info@safespeak.com</a>.
+    <br><br>
+    <span style="font-size: 20px;">Stay updated with our latest developments and research</span>
+</div>
+""", unsafe_allow_html=True)
+
+# Social Media Icons
+st.markdown("""
+    <div style="display: flex; justify-content: center; gap: 10px; margin-top: 10px;">
+        <a href="https://twitter.com/safespeak" target="_blank">
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/twitter.png" style="width:35px;height:35px;">
+        </a>
+        <a href="https://www.linkedin.com/company/safespeak" target="_blank">
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/linkedin.png" style="width:35px;height:35px;">
+        </a>
+        <a href="https://www.facebook.com/safespeak" target="_blank">
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/facebook.png" style="width:35px;height:35px;">
+        </a>
+    </div>
+""", unsafe_allow_html=True)
